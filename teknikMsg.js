8d4b5e782995b67fc59e5deddef11348212c6042
Leo <leonardonifinluri@gmail.com>
2024-09-23 11:01:32 +0800

    This is modification using vite

--- teknikMsg.js
+++ teknikMsg.js
@@ -12,7 +12,6 @@ const teknikMsg = (element1, element2) => {
         "KEEP ON FIGHTING TILL THE END",
         "NOW AND FOREVER!",
         "YEAH!",
-        "YEAH!",
         "YEAH!"
     ];
     const n = message.length
@@ -24,8 +23,14 @@ const teknikMsg = (element1, element2) => {
     //create the logic of the button
     element1.click(() => {
         counter = counter % n
-        element2.text(message[counter])
-        element1.text(message[++counter])
+        if(counter >= 6){
+            element2.text('NOW AND FOREVER!')
+            element1.text('YEAH!')
+            counter++
+        }else{
+            element2.text(message[counter])
+            element1.text(message[++counter])
+        }
     })
 }
 
